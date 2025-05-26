e39da2642b3fae505956a72624870ea6987390f3
Amir11f <zohrabiamirhossain@gmail.com>
2025-05-26 11:36:53 +0300

    Revert "Revert "adding .env file""
    This reverts commit e63c8a6617077161fb027fcd8a641a44346d2371.

--- src/Component/movieCardExpand/peopleForMovieCardExpend.jsx
+++ src/Component/movieCardExpand/peopleForMovieCardExpend.jsx
@@ -1,26 +1,35 @@
-import nullImage from '../../assets/1.png'
+import nullImage from "../../assets/1.png";
 
+const defaultImg = nullImage;
 
-const defaultImg = nullImage
-
-function andetc (pra){
+function andetc(pra) {
   if (pra.length > 25) {
     return pra.substring(0, 24) + "...";
-  }else{
-    return pra
+  } else {
+    return pra;
   }
 }
 
-function PeopleForMovieCardExpend({actors}) {
+function PeopleForMovieCardExpend({ actors }) {
   return (
-    <div className='expend-actors'>
-        <img src={actors.profile_path ? `https://image.tmdb.org/t/p/w500/${actors.profile_path}` : defaultImg } alt={actors.name} className='ex-img-actor' />
-        <div className="realName">
-          <p className='realName-p1'>{actors.name}</p>
-          <p className='realName-p2'>{actors.character && andetc(actors.character)}</p>
-        </div>
+    <div className="expend-actors">
+      <img
+        src={
+          actors.profile_path
+            ? `https://image.tmdb.org/t/p/w154/${actors.profile_path}`
+            : defaultImg
+        }
+        alt={actors.name}
+        className="ex-img-actor"
+      />
+      <div className="realName">
+        <p className="realName-p1">{actors.name}</p>
+        <p className="realName-p2">
+          {actors.character && andetc(actors.character)}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PeopleForMovieCardExpend
+export default PeopleForMovieCardExpend;
